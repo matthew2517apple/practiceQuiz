60368e2d07ebf17e3289ab32fbcc0f43b2d4016a
matthew <matthew@matthew.com>
2019-01-21 16:16:58 -0600

    Initial Commit

---
+++ practiceQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  practiceQuiz
+//
+//  Created by MJC-iCloud on 1/21/19.
+//  Copyright © 2019 Matthew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
