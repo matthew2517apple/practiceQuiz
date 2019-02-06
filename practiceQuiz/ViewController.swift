2cf68dc40ffd6f4db4badd6469074d42748831f2
matthew <matthew@matthew.com>
2019-02-05 21:56:29 -0600

    Everything working, but no app icon.

--- practiceQuiz/ViewController.swift
+++ practiceQuiz/ViewController.swift
@@ -13,8 +13,41 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        questionLabel.text = myQuestions[iCurrentQuestion]
     }
 
-
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+        iCurrentQuestion += 1
+        if (iCurrentQuestion == myQuestions.count) {
+            iCurrentQuestion = 0;
+        }
+        let myQuestion: String = myQuestions[iCurrentQuestion];
+        questionLabel.text = myQuestion
+        // Clear the answer text:
+        answerLabel.text = "???"
+    }
+    
+    @IBAction func showAnswer(_ sender: UIButton) {
+        let myAnswer: String = myAnswers[iCurrentQuestion]
+        answerLabel.text = myAnswer
+    }
+    
+    
+    // "let" for constants:
+    let myQuestions: [String] = [
+        "What is 7+7?",
+        "What is the capital of Vermont?",
+        "What is cognac made from?"
+    ]
+    let myAnswers: [String] = [
+        "14",
+        "Montpelier",
+        "Grapes"
+    ]
+    // "var" for variables:
+    var iCurrentQuestion: Int = 0
 }
 
